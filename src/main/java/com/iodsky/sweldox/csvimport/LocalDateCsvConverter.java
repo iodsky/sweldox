1b7d6756f2bd7627fffafd20d3b47750d1e098ff
ibrahim <iharby2002@gmail.com>
2026-01-19 15:38:41 +0800

    refactor(csvimport): Update LocalDateCsvConverter

--- src/main/java/com/iodsky/sweldox/csvimport/LocalDateCsvConverter.java
+++ src/main/java/com/iodsky/sweldox/csvimport/LocalDateCsvConverter.java
@@ -7,12 +7,24 @@ import java.time.format.DateTimeFormatter;
 
 public class LocalDateCsvConverter extends AbstractBeanField<LocalDate, String> {
 
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private static final DateTimeFormatter[] FORMATTERS = {
+            DateTimeFormatter.ofPattern("yyyy-MM-dd"),
+            DateTimeFormatter.ofPattern("MM-dd-yyyy"),
+            DateTimeFormatter.ofPattern("dd-MM-yyyy"),
+            DateTimeFormatter.ofPattern("yyyy/MM/dd"),
+            DateTimeFormatter.ofPattern("MM/dd/yyyy"),
+            DateTimeFormatter.ofPattern("dd/MM/yyyy")
+    };
 
     @Override
     protected LocalDate convert(String value) {
         if (value == null || value.isBlank()) return null;
-        return LocalDate.parse(value.trim(), FORMATTER);
+        for (var formatter : FORMATTERS) {
+            try {
+                    return LocalDate.parse(value.trim(), formatter);
+            } catch (Exception ignored) {}
+        }
+        throw new IllegalArgumentException("An error has occurred while parsing date: " + value);
     }
 
 }
