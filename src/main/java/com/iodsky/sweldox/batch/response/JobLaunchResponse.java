e85ff8fbaccf0d70bd36e9e03892ed05016becd3
ibrahim <iharby2002@gmail.com>
2026-01-22 16:00:52 +0800

    Feat: add JsonInclude to JobLaunchResponse
    - annotated JobLaunchResponse with @JsonInclude(JsonInclude.Include.NON_NULL) to exclude null fields from JSON serialization.

--- src/main/java/com/iodsky/sweldox/batch/response/JobLaunchResponse.java
+++ src/main/java/com/iodsky/sweldox/batch/response/JobLaunchResponse.java
@@ -1,5 +1,6 @@
 package com.iodsky.sweldox.batch.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class JobLaunchResponse {
     private Long jobExecutionId;
     private String fileName;
